f130c9e213c3b3500aaa44fe259df2ba8a9e5a0f
RafaelMz1 <rafaelmartinez.zarate2203@gmail.com>
2026-02-25 17:20:14 -0500

    Se agrega la libreria random

--- Ruleta.py
+++ Ruleta.py
@@ -3,3 +3,6 @@ lista1= ["Manzana", "Pera", "Uva", "Mango", "Piña"]
 lista2= ["Manzana", "Pera", "Uva", "Mango", "Piña"]
 lista3= ["Manzana", "Pera", "Uva", "Mango", "Piña"]
 
+random_list1 = random.choice(lista1)
+random_list2 = random.choice(lista2)
+random_list3 = random.choice(lista3)
