7146e0a6887794ecd152aaa84d042b5246a428c5
RafaelMz1 <rafaelmartinez.zarate2203@gmail.com>
2026-02-25 17:58:16 -0500

    Se agrega el tiempo y las variables if del juego

--- Ruleta.py
+++ Ruleta.py
@@ -1,15 +1,29 @@
-import random
-lista1= ["Manzana", "Pera", "Uva", "Mango", "Piña"]
-lista2= ["Manzana", "Pera", "Uva", "Mango", "Piña"]
-lista3= ["Manzana", "Pera", "Uva", "Mango", "Piña"]
+import random,time
+while True:
+    print("Bienvenido al juego de la ruleta")
+    lista1= ["Manzana", "Pera", "Uva", "Mango", "Piña"]
+    lista2= ["Manzana", "Pera", "Uva", "Mango", "Piña"]
+    lista3= ["Manzana", "Pera", "Uva", "Mango", "Piña"]
 
-random_list1 = random.choice(lista1)
-random_list2 = random.choice(lista2)
-random_list3 = random.choice(lista3)
-print(random_list1,random_list2,random_list3)
+    random_list1 = random.choice(lista1)
+    random_list2 = random.choice(lista2)
+    random_list3 = random.choice(lista3)
+    inicio = time.time()
+    print(random_list1,random_list2,random_list3)
+    if random_list1 == random_list2 == random_list3:
+        print("\nGANASTE\n")
+        final = time.time()
+        diferencia = final-inicio
+        print(f"te demoraste {diferencia} segundos")
+        print("\nQuieres jugar de nuevo?")
+        opcion = input(':').lower()
+        if opcion == "si":
+            print()
+        else:
+            print("esta bien")
+            break
 
-if random_list1 == random_list2 and random_list2 == random_list3:
-    print("GANASTE")
+    else:
+        print("\nPERDISTE\n")
+        time.sleep(1)
 
-else:
-    print("PERDISTE")
